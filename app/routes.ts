7ce059481f8c32c636ea8305c27eb0bc01accd4a
TkTioNG <tktiong@icloud.com>
2025-05-10 15:54:12 +0800

    feat: add anime detail route

--- app/routes.ts
+++ app/routes.ts
@@ -2,4 +2,5 @@ import { type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
   route("/", "routes/home.tsx", [route("/", "routes/anime/animeList.tsx")]),
+  route("/anime/:id", "routes/anime/animeDetail.tsx"),
 ] satisfies RouteConfig;
